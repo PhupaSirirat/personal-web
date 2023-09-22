fee1e28338aa94694f800760a9314be3e2147818
Phupa S <99344705+PhupaSirirat@users.noreply.github.com>
2023-09-23 01:45:12 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1440px',
+    },
+    colors: {
+      prinmary: '#03001C',
+      secondary: '#301E67',
+      tertiary: '#5B8FB9',
+      quaternary: '#B6EADA',
+    },
     extend: {},
   },
   plugins: [],
